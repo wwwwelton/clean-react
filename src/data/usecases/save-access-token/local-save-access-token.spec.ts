d6f607c6a73dc587e6be039b081ce48c064e32f1
avatar9966 <av4t@hotmail.com>
2021-03-30 20:26:50 -0300

    refactor: rename data mocks

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,5 +1,5 @@
-import { SetStorageMock } from '@/data/test/mock-storage'
 import { LocalSaveAccessToken } from './local-save-access-token'
+import { SetStorageMock } from '@/data/test'
 import faker from 'faker'
 
 type SutTypes = {
